57a6e9e4c9190e98571144910ca980c2069e0988
Tomasz Kędziorek <tomasz.kedziorek.programista@gmail.com>
2022-01-03 11:45:59 +0100

    add User class

---
+++ user.cpp
@@ -0,0 +1,46 @@
+#include "user.h"
+
+using namespace std;
+
+User::User( int userID, string name,string surname,string login, string password ) {
+        this -> userID = userID;
+        this -> name = name;
+        this -> surname = surname;
+        this -> login = login;
+        this -> password = password;
+}
+
+void User::setLogin( string userLogin ) {
+    login = userLogin;
+}
+
+void User::setPassword( string userPassword ) {
+    password = userPassword;
+}
+
+void User::setID( int ID_user ) {
+    userID = ID_user;
+}
+void User::setName( string userName ){
+    name = userName;
+}
+void User::setSurname( string userSurname ){
+    surname = userSurname;
+}
+
+string User::getLogin() {
+    return login;
+}
+
+string User::getPassword() {
+    return password;
+}
+int User::getUserID() {
+    return userID;
+}
+
+void User::showUserData() {
+    cout<< login << " " << password <<endl;
+    cout<< name << " " <<surname <<endl;
+    cout<< "ID: "<< userID <<endl;
+}
---
+++ user.h
@@ -0,0 +1,28 @@
+#ifndef USER_H
+#define USER_H
+#include <iostream>
+
+using namespace std;
+
+class User {
+    string login;
+    string password;
+    string name ;
+    string surname;
+    int userID;
+
+public:
+    User(int userID =0, string name ="",string surname ="",string login ="",string password ="" );
+    void setLogin( string userlogin );
+    void setPassword( string userPassword );
+    void setName( string userName );
+    void setSurname( string userSurname );
+    void setID( int ID_user );
+    string getLogin();
+    string getPassword();
+    int getUserID();
+    void showUserData();
+};
+
+
+#endif // USER_H
