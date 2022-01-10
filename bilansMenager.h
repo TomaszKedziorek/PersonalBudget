129f8f79da80ce623191e88e76f2e2d59a0d8dab
Tomasz Kędziorek <tomasz.kedziorek.programista@gmail.com>
2022-01-10 12:29:43 +0100

    add bilansMenager class

---
+++ bilansMenager.h
@@ -0,0 +1,17 @@
+#ifndef BILANSMENAGER_H
+#define BILANSMENAGER_H
+#include <iostream>
+#include <vector>
+#include "date.h"
+#include "commonFunk.h"
+#include "inexMenager.h"
+using namespace std;
+
+class BilansMenager{
+
+public:
+
+};
+
+
+#endif // BILANSMENAGER_H
